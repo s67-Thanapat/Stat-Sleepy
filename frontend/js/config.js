9929252d78c1dff4bda398ff60dfc456a294773b
Thanas <s6701012610180@email.kmutnb.ac.th>
2025-09-24 09:49:55 +0700

    allnews3

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -4,7 +4,7 @@
 window.CONFIG = {
   // จาก Supabase > Project Settings > API
   SUPABASE_URL: 'https://nbxuzdrutlvavirpzpjt.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9....',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ieHV6ZHJ1dGx2YXZpcnB6cGp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgzODUzMDYsImV4cCI6MjA3Mzk2MTMwNn0.tD4noFRKDFcA9x-A5udQSjkhKuXbccPdohdFjd8Fyo4',
 
   // ถ้ามี backend แยก กำหนดไว้ที่นี่
   BACKEND_URL: 'https://stat-sleepy-backend.vercel.app'
